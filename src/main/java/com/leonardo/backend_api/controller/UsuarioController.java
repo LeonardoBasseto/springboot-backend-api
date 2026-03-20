f473b6862726aed9e3992312cdc0501f4fa5ecd1
Leonardo Basseto <leobasseto0910@gmail.com>
2026-03-20 17:02:57 -0300

    refactor(usuario): remove unnecessary endpoints

--- src/main/java/com/leonardo/backend_api/controller/UsuarioController.java
+++ src/main/java/com/leonardo/backend_api/controller/UsuarioController.java
@@ -1,13 +1,7 @@
 package com.leonardo.backend_api.controller;
 
-import java.util.List; 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,34 +23,6 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
-	@Operation(summary = "Listar todos os usuários", description = "Retorna uma lista com todos os usuários cadastrados")
-	@ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
-	@GetMapping
-	public List<UsuarioDTO> listarTodos(){
-	return usuarioService.listarTodos();
-	}
-	
-	@Operation(summary = "Buscar usuários por ID", description = "Retorna o ID do usuário especificado")
-	@ApiResponses({
-	@ApiResponse(responseCode = "200", description = "usuário encontrado"),
-	@ApiResponse(responseCode = "404", description = "usuário não encontrado")
-	})
-	@GetMapping("/{id}")
-	public UsuarioDTO buscarPorId(@PathVariable Long id){
-	    return usuarioService.buscarPorId(id);
-	}
-	
-	@Operation(summary = "Inserir usuários", description = "Cria um novo usuário")
-	@ApiResponses({
-	@ApiResponse(responseCode = "200", description = "usuário criado com sucesso"),
-	@ApiResponse(responseCode = "400", description = "Dados inválidos")
-	})
-	@PostMapping
-	public ResponseEntity<UsuarioDTO> inserir(@RequestBody UsuarioDTO usuario) {
-	    UsuarioDTO novo = usuarioService.inserir(usuario);
-	    return ResponseEntity.status(201).body(novo);
-	}	
-	
 	@Operation(summary = "Alterar usuário", description = "Altera os dados de um usuário já existente")
 	@ApiResponses({
 	@ApiResponse(responseCode = "200", description = "usuário alterado"),
@@ -65,16 +31,5 @@ public class UsuarioController {
 	@PutMapping("/{id}")
 	public UsuarioDTO alterar(@PathVariable Long id,@RequestBody UsuarioDTO usuario) {
 		return usuarioService.alterar(id, usuario);
-	}
-	
-	@Operation(summary = "Excluir usuário", description = "Excluir os dados de um usuário")
-	@ApiResponses({
-	@ApiResponse(responseCode = "200", description = "usuário excluido"),
-	@ApiResponse(responseCode = "404", description = "usuário não encontrado")
-	})
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> excluir (@PathVariable Long id){
-		usuarioService.excluir(id);
-		return ResponseEntity.ok().build();
 	}
 }
